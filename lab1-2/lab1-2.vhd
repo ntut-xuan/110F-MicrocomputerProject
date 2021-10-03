6de68e85d9ca8921ef46ca23945340eb2e49c7b4
ntut-xuan <t109590031@ntut.org.tw>
2021-10-03 16:18:46 +0800

    update(2021/10/03)

--- lab1-2/lab1-2.vhd
+++ lab1-2/lab1-2.vhd
@@ -8,131 +8,71 @@ end seven_segment;
 
 architecture archi of seven_segment is
 begin
-process(W,X,Y,Z)
-	variable t1: Integer range 0 to 16 := 0;
-	variable t2: Integer range 0 to 16 := 0;
+	process(W,X,Y,Z)
+	variable t: Integer range 0 to 16;
 	begin
-		t1 := 0;
+		for i in 0 to 1 loop
+			
+			-- init the t, let t reset to 0
+			t := 0;
 
-		-- calculate WXYZ and turn it into integer
-		if W(0) = '1' then
-			t1 := t1 + 8;
-		end if;
-		if X(0) = '1' then
-			t1 := t1 + 4;
-		end if;
-		if Y(0) = '1' then
-			t1 := t1 + 2;
-		end if;
-		if Z(0) = '1' then
-			t1 := t1 + 1;
-		end if;
+			-- calculate WXYZ and turn it into integer
+			if W(i) = '1' then
+				t := t + 8;
+			end if;
+			if X(i) = '1' then
+				t := t + 4;
+			end if;
+			if Y(i) = '1' then
+				t := t + 2;
+			end if;
+			if Z(i) = '1' then
+				t := t + 1;
+			end if;
 
-		-- reset led.
-		a(0) <= '0';
-		b(0) <= '0';
-		c(0) <= '0';
-		d(0) <= '0';
-		e(0) <= '0';
-		f(0) <= '0';
-		g(0) <= '0';
+			-- reset led.
+			a(i) <= '0';
+			b(i) <= '0';
+			c(i) <= '0';
+			d(i) <= '0';
+			e(i) <= '0';
+			f(i) <= '0';
+			g(i) <= '0';
 
-		-- if t={1,4,11,13}, turn off a-LED.
-		if t1 = 1 or t1 = 4 or t1 = 11 or t1 = 13 then
-			a(0) <= '1';
-		end if;
+			-- if t={1,4,11,13}, turn off a-LED.
+			if t = 1 or t = 4 or t = 11 or t = 13 then
+				a(i) <= '1';
+			end if;
 
-		-- if t={5,6,11,12,14,15}, turn off b-LED.
-		if t1 = 5 or t1 = 6 or t1 = 11 or t1 = 12 or t1 = 14 or t1 = 15 then
-			b(0) <= '1';
-		end if;
+			-- if t={5,6,11,12,14,15}, turn off b-LED.
+			if t = 5 or t = 6 or t = 11 or t = 12 or t = 14 or t = 15 then
+				b(i) <= '1';
+			end if;
 
-		-- if t={2,12,14,15}, turn off c-LED.
-		if t1 = 2 or t1 = 12 or t1 = 14 or t1 = 15 then
-			c(0) <= '1';
-		end if;
+			-- if t={2,12,14,15}, turn off c-LED.
+			if t = 2 or t = 12 or t = 14 or t = 15 then
+				c(i) <= '1';
+			end if;
 
-		-- if t={1, 4, 7, 9, 10, 15}, turn off d-LED
-		if t1 = 1 or t1 = 4 or t1 = 7 or t1 = 9 or t1 = 10 or t1 = 15 then
-			d(0) <= '1';
-		end if;
+			-- if t={1, 4, 7, 9, 10, 15}, turn off d-LED
+			if t = 1 or t = 4 or t = 7 or t = 9 or t = 10 or t = 15 then
+				d(i) <= '1';
+			end if;
 
-		-- if t={1, 3, 4, 5, 7, 9}, turn off e-LED
-		if (t1 = 1 or t1 = 3 or t1 = 4 or t1 = 5 or t1 = 7 or t1 = 9) then
-			e(0) <= '1';
-		end if;
+			-- if t={1, 3, 4, 5, 7, 9}, turn off e-LED
+			if (t = 1 or t = 3 or t = 4 or t = 5 or t = 7 or t = 9) then
+				e(i) <= '1';
+			end if;
 
-		-- if t={1, 2, 3, 7, 13}, turn off f-LED
-		if (t1 = 1 or t1 = 2 or t1 = 3 or t1 = 7 or t1 = 13) then
-			f(0) <= '1';
-		end if;
+			-- if t={1, 2, 3, 7, 13}, turn off f-LED
+			if (t = 1 or t = 2 or t = 3 or t = 7 or t = 13) then
+				f(i) <= '1';
+			end if;
 
-		-- if t={0, 1, 7, 12}, turn off g-LED
-		if (t1 = 0 or t1 = 1 or t1 = 7 or t1 = 12) then
-			g(0) <= '1';
-		end if;
-
-		t2 := 0;
-
-		-- calculate WXYZ and turn it into integer
-		if W(1) = '1' then
-			t2 := t2 + 8;
-		end if;
-		if X(1) = '1' then
-			t2 := t2 + 4;
-		end if;
-		if Y(1) = '1' then
-			t2 := t2 + 2;
-		end if;
-		if Z(1) = '1' then
-			t2 := t2 + 1;
-		end if;
-
-		-- reset led.
-		a(1) <= '0';
-		b(1) <= '0';
-		c(1) <= '0';
-		d(1) <= '0';
-		e(1) <= '0';
-		f(1) <= '0';
-		g(1) <= '0';
-
-		-- if t={1,4,11,13}, turn off a-LED.
-		if t2 = 1 or t2 = 4 or t2 = 11 or t2 = 13 then
-			a(1) <= '1';
-		end if;
-
-		-- if t={5,6,11,12,14,15}, turn off b-LED.
-		if t2 = 5 or t2 = 6 or t2 = 11 or t2 = 12 or t2 = 14 or t2 = 15 then
-			b(1) <= '1';
-		end if;
-
-		-- if t={2,12,14,15}, turn off c-LED.
-		if t2 = 2 or t2 = 12 or t2 = 14 or t2 = 15 then
-			c(1) <= '1';
-		end if;
-
-		-- if t={1, 4, 7, 9, 10, 15}, turn off d-LED
-		if t2 = 1 or t2 = 4 or t2 = 7 or t2 = 9 or t2 = 10 or t2 = 15 then
-			d(1) <= '1';
-		end if;
-
-		-- if t={1, 3, 4, 5, 7, 9}, turn off e-LED
-		if (t2 = 1 or t2 = 3 or t2 = 4 or t2 = 5 or t2 = 7 or t2 = 9) then
-			e(1) <= '1';
-		end if;
-
-		-- if t={1, 2, 3, 7, 13}, turn off f-LED
-		if (t2 = 1 or t2 = 2 or t2 = 3 or t2 = 7 or t2 = 13) then
-			f(1) <= '1';
-		end if;
-
-		-- if t={0, 1, 7, 12}, turn off g-LED
-		if (t2 = 0 or t2 = 1 or t2 = 7 or t2 = 12) then
-			g(1) <= '1';
-		end if;
-
-		-- project done.
-		
+			-- if t={0, 1, 7, 12}, turn off g-LED
+			if (t = 0 or t = 1 or t = 7 or t = 12) then
+				g(i) <= '1';
+			end if;
+		end loop;
 	end process;
 end archi;
