42007c8556463f2695f20d1fca2d1f60d2bf093a
ntut-xuan <t109590031@ntut.org.tw>
2021-10-14 10:53:16 +0800

    update(2021/10/14)

--- lab2-1/main.vhd
+++ lab2-1/main.vhd
@@ -5,7 +5,7 @@ use work.seven_segment_package.all;
 
 entity fulladder is
 	port(
-	a, b: in std_logic_vector(0 to 7); 
+	input_a, input_b: in std_logic_vector(7 downto 0); 
 	cout: out std_logic; 
 	l1: out std_logic_vector(0 to 6);
 	l2: out std_logic_vector(0 to 6)
@@ -19,17 +19,18 @@ end component;
 component sevenSegment
 	port(w, x, y, z: in std_logic; a, b, c, d, e, f, g: out std_logic);
 end component;
-signal s: std_logic_vector(0 to 7);
-signal c: std_logic_vector(0 to 7);
+signal sum: std_logic_vector(0 to 7);
+signal carry: std_logic_vector(0 to 7);
+
 begin
-	adder1: fulladd port map(c(0),  a(0), b(0), c(1), s(0));
-	adder2: fulladd port map(c(1), a(1), b(1), c(2), s(1));
-	adder3: fulladd port map(c(2), a(2), b(2), c(3), s(2));
-	adder4: fulladd port map(c(3), a(3), b(3), c(4), s(3));
-	adder5: fulladd port map(c(4), a(4), b(4), c(5), s(4));
-	adder6: fulladd port map(c(5), a(5), b(5), c(6), s(5));
-	adder7: fulladd port map(c(6), a(6), b(6), c(7), s(6));
-	adder8: fulladd port map(c(7), a(7), b(7), cout, s(7));
-	led1: sevenSegment port map(s(0), s(1), s(2), s(3), l1(0), l1(1), l1(2), l1(3), l1(4), l1(5), l1(6));
-	led2: sevenSegment port map(s(4), s(5), s(6), s(7), l2(0), l2(1), l2(2), l2(3), l2(4), l2(5), l2(6));
+	adder1: fulladd port map(carry(0), input_a(0), input_b(0), sum(0), carry(1));
+	adder2: fulladd port map(carry(1), input_a(1), input_b(1), sum(1), carry(2));
+	adder3: fulladd port map(carry(2), input_a(2), input_b(2), sum(2), carry(3));
+	adder4: fulladd port map(carry(3), input_a(3), input_b(3), sum(3), carry(4));
+	adder5: fulladd port map(carry(4), input_a(4), input_b(4), sum(4), carry(5));
+	adder6: fulladd port map(carry(5), input_a(5), input_b(5), sum(5), carry(6));
+	adder7: fulladd port map(carry(6), input_a(6), input_b(6), sum(6), carry(7));
+	adder8: fulladd port map(carry(7), input_a(7), input_b(7), sum(7), cout);
+	led1: sevenSegment port map(sum(0), sum(1), sum(2), sum(3), l1(0), l1(1), l1(2), l1(3), l1(4), l1(5), l1(6));
+	led2: sevenSegment port map(sum(4), sum(5), sum(6), sum(7), l2(0), l2(1), l2(2), l2(3), l2(4), l2(5), l2(6));
 end adder;
---
+++ lab2-1/seven_segment_package.vhd.bak
@@ -0,0 +1,8 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package seven_segment_package is
+	component sevenSegment
+		port(w, x, y, z: in std_logic; a, b, c, d, e, f, g: out std_logic);
+	end component;
+end seven_segment_package;
