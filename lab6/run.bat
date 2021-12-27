d62b36b7392c153202c314b3e866d0dcd2981274
黃漢軒 <t109590031@ntut.edu.tw>
2021-12-27 22:48:15 +0800

    update(2021/12/27)

--- lab6/run.bat
+++ lab6/run.bat
@@ -1,4 +1,8 @@
-C:\intelFPGA_lite\18.1\quartus\bin64\quartus_map --read_settings_files=on --write_settings_files=off lab6 -c 8bit-div
-C:\intelFPGA_lite\18.1\quartus\bin64\quartus_fit --read_settings_files=off --write_settings_files=off lab6 -c 8bit-div
-C:\intelFPGA_lite\18.1\quartus\bin64\quartus_sta lab6 -c 8bit-div
-C:\intelFPGA_lite\18.1\quartus\bin64\quartus_sh -t c:/intelfpga_lite/18.1/quartus/common/tcl/internal/nativelink/qnativesim.tcl --block_on_gui lab6 8bit-div
+ghdl -a --std=02 --ieee=synopsys shift_register_package.vhd
+ghdl -a --std=02 --ieee=synopsys main.vhd
+ghdl -a --std=02 --ieee=synopsys subtractor_package.vhd
+ghdl -a --std=02 --ieee=synopsys subtractor.vhd
+ghdl -a --std=02 --ieee=synopsys lab6.vhd
+ghdl -e --std=02 --ieee=synopsys eight_bits_div 
+ghdl -r --std=02 --ieee=synopsys eight_bits_div --vcd=lab6.vcd
+pause
