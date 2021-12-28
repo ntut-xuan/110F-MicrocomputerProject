cb9cbb22cf1ad03ff460921dae2f746fd86e095a
ntut-xuan <t109590031@ntut.org.tw>
2021-12-28 13:56:41 +0800

    update(2021/12/28)

--- lab6/run.bat
+++
@@ -1,8 +0,0 @@
-ghdl -a --std=02 --ieee=synopsys shift_register_package.vhd
-ghdl -a --std=02 --ieee=synopsys main.vhd
-ghdl -a --std=02 --ieee=synopsys subtractor_package.vhd
-ghdl -a --std=02 --ieee=synopsys subtractor.vhd
-ghdl -a --std=02 --ieee=synopsys lab6.vhd
-ghdl -e --std=02 --ieee=synopsys eight_bits_div 
-ghdl -r --std=02 --ieee=synopsys eight_bits_div --vcd=lab6.vcd
-pause
